83a7b5c02c8b4b81a3c4ea3b7f1a80612b18e984
Jacob Daitzman <jacob@I7-Imac.local>
2016-02-10 18:28:25 -0500

    fixed bug in query.js

--- www/lib/js/query.js
+++ www/lib/js/query.js
@@ -1,5 +1,6 @@
 var query = function() {
   var obj = {};
+  if (window.location.href.split('?').length <= 1) return obj;
   var str = window.location.href.split('?')[1];
   var arr = str.split('&');
   for (var i in arr) {
