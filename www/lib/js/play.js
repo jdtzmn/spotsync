c6090c696f950c468e950dfd4f03a975a503b764
jdtzmn <jdtzmn@gmail.com>
2016-02-16 09:29:55 -0500

    updated play.js

--- www/lib/js/play.js
+++ www/lib/js/play.js
@@ -2,7 +2,18 @@ var play = {
   format: function(num) {
     return Math.floor(num / 60) + ':' + '0'.substring(0, 2 - JSON.stringify(num % 60).length) + num % 60;
   },
+  time: function() {
+    return new Date().getTime();
+  },
   user: function(id) {
-    socket.emit('status', id);
+    socket.emit('status', id, function(status) {
+      console.log(status);
+    });
+  },
+  track: function(id, from, cb) {
+
+  },
+  playing: function(id, from) {
+
   }
 };
