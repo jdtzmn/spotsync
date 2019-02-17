2970759527ff1232e6abf023f0f88eee46a089eb
jdtzmn <jdtzmn@gmail.com>
2019-02-16 20:38:43 -0500

    Added dist/ to .gitignore

--- dist/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const next_routes_1 = __importDefault(require("next-routes"));
-exports.default = new next_routes_1.default()
-    .add('party', '/party/:id');
-//# sourceMappingURL=routes.js.map
